af3e24838149e607c6b3b5a3a5d88ecda34da6ea
Hyeonil Na <sjrnfl528@gmail.com>
2022-01-11 14:13:15 +0900

    커스텀훅 예시 추가 (useCurrentTime, useWindowWidth)

--- src/App.js
+++ src/App.js
@@ -2,28 +2,38 @@ import TopNav from 'components/TopNav';
 import Login from 'pages/accounts/Login';
 import Profile from 'pages/accounts/Profile';
 import PageBlog from 'pages/blog/PageBlog';
+import Clock from 'pages/examples/Clock';
 import Components from 'pages/examples/Components';
 import ReviewForm from 'pages/reviews/ReviewForm';
 import ReviewList from 'pages/reviews/ReviewList';
+import useWindowWidth from 'pages/examples/useWindowWidth';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
+  const windowWidth = useWindowWidth();
   return (
-    <div className="app">
-      <TopNav />
+    <>
+      <div className="app">
+        <TopNav />
+        <Routes>
+          <Route path="/" element={<Navigate to="/blog/" />} />
+          <Route path="/accounts/login/" element={<Login />} />
+          <Route path="/accounts/profile/" element={<Profile />} />
+          <Route path="/blog/" element={<PageBlog />} />
+          <Route path="/reviews/" element={<ReviewList />} />
+          <Route path="/reviews/new/" element={<ReviewForm />} />
+          <Route path="/reviews/:reviewId/edit/" element={<ReviewForm />} />
+          <Route path="/examples/components/" element={<Components />} />
+        </Routes>
+      </div>
       <Routes>
-        <Route path="/" element={<Navigate to="/reviews/" />} />
-        <Route path="/accounts/login/" element={<Login />} />
-        <Route path="/accounts/profile/" element={<Profile />} />
-        <Route path="/reviews/" element={<ReviewList />} />
-        <Route path="/reviews/new" element={<ReviewForm />} />
-        <Route path="/review/:reviewId/edit/" element={<ReviewForm />} />
-        <Route path="/examples/components/" element={<Components />} />
-        <Route path="/blog/" element={<PageBlog />} />
+        <Route path="/examples/clock/" element={<Clock />} />
       </Routes>
-    </div>
+      <hr />
+      윈도우 가로크기 : {windowWidth}px
+    </>
   );
 }
 
